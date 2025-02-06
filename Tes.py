9582a08448bdd7bbfc97f43ca821c1a485f0550e
genotip2 <genotip2@gmail.com>
2025-02-06 22:48:10 +0700

    Update Tes.py

--- Tes.py
+++ Tes.py
@@ -1,11 +1,15 @@
-from tradingview_ta import TA_Handler
+from tradingview_ta import TA_Handler, Interval
+
+symbol = "BTCUSDT"  # Ganti dengan simbol yang ingin dicek
 
 handler = TA_Handler(
-            symbol=symbol,
-            exchange="BINANCE",
-            screener="CRYPTO",
-            interval=Interval.INTERVAL_15_MINUTES
-        ).get_analysis()
+    symbol=symbol,
+    exchange="BINANCE",
+    screener="CRYPTO",
+    interval=Interval.INTERVAL_15_MINUTES
+)
 
-# Cetak semua key indikator
+analysis = handler.get_analysis()
+
+# Cetak semua indikator yang tersedia
 print(analysis.indicators.keys())
